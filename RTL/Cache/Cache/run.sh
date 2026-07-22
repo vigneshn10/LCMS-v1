8cce2781feb1cb08b3c586f88feca7f6965ca8f0
vigneshn10 <105668570+vigneshn10@users.noreply.github.com>
2026-07-22 14:37:28 -0700

    Delete RTL/Cache directory

--- RTL/Cache/Cache/run.sh
+++
@@ -1,5 +0,0 @@
-cd /home/runner
-export PATH=/usr/bin:/bin:/tool/pandora64/bin:/usr/local/bin
-export EDATOOL=icarus
-export HOME=/home/runner
-export SIM=icarus; python3 testbench.py ; echo 'Creating result.zip...' && zip -r /tmp/tmp_zip_file_123play.zip . && mv /tmp/tmp_zip_file_123play.zip result.zip
